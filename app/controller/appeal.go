108a726bdceb0b97fc0fe9a06087a6ddc0fe2fb7
Brady Walsh <walshb8@wit.edu>
2018-02-12 11:52:56 -0500

    Added ability to pull up specific Appeals

--- app/controller/appeal.go
+++ app/controller/appeal.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	"appeals/app/model"
 	"appeals/app/view"
 	"html/template"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type appeal struct {
@@ -11,9 +15,23 @@ type appeal struct {
 }
 
 func (a appeal) registerRoutes() {
-	http.HandleFunc("/appeal", a.handleAppeal)
+	http.HandleFunc("/appeal/", a.handleAppeal)
 }
 
 func (a appeal) handleAppeal(w http.ResponseWriter, r *http.Request) {
-	vm := view.NewAppeal()
+	appealPattern, _ := regexp.Compile(`/appeal/(\d+)`)
+	match := appealPattern.FindStringSubmatch(r.URL.Path)
+	appeal, _ := strconv.Atoi(match[1])
+	a.handleAppellant(w, r, appeal)
+}
+
+func (a appeal) handleAppellant(w http.ResponseWriter, r *http.Request, appealId int) {
+	appeal, err := model.GetAppealByID(appealId)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Appeal not found", http.StatusNotFound)
+	}
+	vm := view.NewAppeal(appeal)
+	w.Header().Add("Content-Type", "text/html")
+	a.appealTemplate.Execute(w, vm)
 }
