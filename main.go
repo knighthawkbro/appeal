30f5175cce8820a63957b8c82f8060c02e060a2d
Brady Walsh <walshb8@wit.edu>
2018-02-12 11:51:43 -0500

    Added some documentation for the Populate template function

--- main.go
+++ main.go
@@ -33,16 +33,20 @@ func connectToDatabase() *sql.DB {
 	return db
 }
 
+// populateTemplates (Private) - Returns a map of templates with filename as
+// the key and the content of the template as the value of the map
 func populateTemplates() map[string]*template.Template {
-	result := make(map[string]*template.Template)
-	const basePath = "templates"
-	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	result := make(map[string]*template.Template)                            // Creates a basic map
+	const basePath = "templates"                                             // The name of the folder for the templates located in the root web server folder
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html")) // Making sure that the layout is loaded first
+	// Then loads all the layout dependancies next
 	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html", basePath+"/_shim.html"))
+	// opens the folder with all the content for the website
 	dir, err := os.Open(basePath + "/content")
 	if err != nil {
-		panic("Failed to open template blocks directory: " + err.Error())
+		panic("Failed to open template blocks directory: " + err.Error()) // exits webserver imediately when the directory is not found.
 	}
-	fis, err := dir.Readdir(-1)
+	fis, err := dir.Readdir(-1) // Reads all the information contained in the w
 	if err != nil {
 		panic("Failed to read contents of content directory: " + err.Error())
 	}
